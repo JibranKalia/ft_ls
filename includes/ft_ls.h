ce323a6d4aa2cfcd2f8bfcb9d4a6474cf33afc7d
Jibran Kalia <jibran.kalia@gmail.com>
2017-04-30 21:26:44 -0700

    It starts

---
+++ includes/ft_ls.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/30 21:25:22 by jkalia            #+#    #+#             */
+/*   Updated: 2017/04/30 21:26:33 by jkalia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <libft.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <grp.h>
+# include <uuid/uuid.h>
+# include <dirent.h>
+# include <time.h>
+# include <sys/xattr.h>
+
+
+#endif
