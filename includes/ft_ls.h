1e17d5e267436a8fad08ff596dbd847a17843b7b
Jibran Kalia <jkalia@student.42.us.org>
2017-05-01 12:19:42 -0700

    Initial work

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,24 +6,40 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/30 21:25:22 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/30 21:48:03 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/01 11:51:37 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
+# include <dirent.h>
+# include <grp.h>
 # include <libft.h>
+# include <pwd.h>
+# include <sys/xattr.h>
 # include <sys/types.h>
 # include <sys/stat.h>
-# include <pwd.h>
-# include <grp.h>
-# include <uuid/uuid.h>
-# include <dirent.h>
 # include <time.h>
-# include <sys/xattr.h>
+# include <uuid/uuid.h>
 
-typedef struct dirent t_dir;
+typedef struct dirent	t_dir;
+typedef struct stat		t_stat;
 
+enum					e_ls_flags
+{
+	FLG_l = 0x1,
+	FLG_R = 0x2,
+	FLG_a = 0x4,
+	FLG_r = 0x8,
+	FLG_t = 0x10
+};
+
+typedef struct			s_ls_file
+{
+	char				*path;
+	char				*name;
+	t_stat				stat;
+}						t_ls_file;
 
 #endif
--- src/ls_main.c
+++ src/ls_main.c
@@ -6,13 +6,50 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/30 21:24:20 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/30 21:25:17 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/01 12:06:10 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 
-int	main(void)
+uint8_t		g_ls_flags = 0;
+
+
+int8_t		ft_ls_print_dir(char *path)
 {
+	t_dir		*dp;
+	t_ls_file	*temp;
+	t_arr		*files;
+	DIR			*dirp
+
+	dirp = opendir(path);
+	CHK(dirp == NULL, -1);
+	files = arr_create(sizeof(t_ls_file), 5);
+	CHECK_MEM(files, RETURN(NULL));
+	while ((dp == readdir(dirp)) != 0)
+	{
+		if (dp->d_name[0] == '.' && !(g_ls_flgs & FLG_a))
+			continue ;
+		tmp = arr_new(files);
+		CHK(tmp == NULL, -1)
+		ft_asprintf(&tmp.path, "%s/%s", path, dp->d_name);
+		tmp.name = dp->d_name;
+		GRD3(lstat(tmp.path, &tmp.info) == -1, free(tmp.path), arr_dtr(files)
+				, closedir(dirp), -1);
+		GRD3(arr_append(files, &tmp) == -1, free(tmp.path), arr_dtr(files)
+				, closedir(dirp), -1);
+		free(tmp.path);
+	}
+	closedir(dirp);
 	return (0);
+
+}
+
+}
+
+int			main(int argc, char **argv)
+{
+	if (argc == 1)
+		ft_ls_print_dir(".");
+
 }
