85b4f972652120210a1bd6320563e8f6c0a2b782
Jibran Kalia <jibran.kalia@gmail.com>
2017-06-25 21:32:52 -0500

    Initial Sort is working

--- src/ls_sort.c
+++ src/ls_sort.c
@@ -6,24 +6,18 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/18 20:24:36 by jkalia            #+#    #+#             */
-/*   Updated: 2017/05/18 20:56:17 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/06/25 21:32:39 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 
-static int	lexcmp(const void *a, const void *b)
-{
-	return (strcmp(((t_ls_file *)(a))->path, ((t_ls_file *)(b))->path));
-}
-
-static int	ls_lexcmp(const void *a, const void *b)
+static int	ls_lexcmp(void *a, void *b)
 {
 	return (ft_strcmp(((t_ls_file *)(a))->path, ((t_ls_file *)(b))->path));
 }
 
 void		ls_sort(t_arr *files)
 {
-	//arr_qsort(files, lexcmp);
-	qsort(files->contents, ARR_COUNT(files), sizeof(void *), ls_lexcmp);
+	ft_qsort(files->contents, 0, ARR_COUNT(files) - 1, ls_lexcmp);
 }
