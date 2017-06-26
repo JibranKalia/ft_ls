630bd2525c973b45ac027cacb8681340d79f42ae
Jibran Kalia <jibran.kalia@gmail.com>
2017-06-25 23:06:21 -0500

    R seems to be working

--- src/ls_sort.c
+++ src/ls_sort.c
@@ -6,11 +6,12 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/18 20:24:36 by jkalia            #+#    #+#             */
-/*   Updated: 2017/06/25 21:32:39 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/06/25 23:03:28 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
+extern uint8_t			g_ls_flags;
 
 static int	ls_lexcmp(void *a, void *b)
 {
@@ -20,4 +21,6 @@ static int	ls_lexcmp(void *a, void *b)
 void		ls_sort(t_arr *files)
 {
 	ft_qsort(files->contents, 0, ARR_COUNT(files) - 1, ls_lexcmp);
+	if (g_ls_flags & FLG_r)
+		arr_reverse(files);
 }
