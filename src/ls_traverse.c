46c947ce511e050f432e0d8a28f26a949558e8d0
Jibran Kalia <jkalia@student.42.us.org>
2017-07-20 20:39:44 -0700

    Fixing traverse

--- src/ls_traverse.c
+++ src/ls_traverse.c
@@ -5,91 +5,122 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/01 11:40:49 by jkalia            #+#    #+#             */
-/*   Updated: 2017/07/01 11:41:13 by jkalia           ###   ########.fr       */
+/*   Created: 2017/06/29 09:07:07 by jkalia            #+#    #+#             */
+/*   Updated: 2017/07/20 20:39:31 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static void		ls_traverse(int argc, char *argv[], int options)
+#include <ft_ls.h>
+
+extern int		g_ls_flags;
+
+static void		print_custom_l(t_arr *fil)
 {
-	FTS *ftsp;
-	FTSENT *p, *chp;
-	int ch_options, error;
+	int		i;
+	int		padding[4];
 
-	if ((ftsp =
-				fts_open(argv, options, g_ls_flg->nosort ? NULL : mastercmp)) == NULL)
-		err(1, "fts_open");
-
-	display(NULL, fts_children(ftsp, 0));
-	if (g_ls_flg->listdir) {
-		fts_close(ftsp);
-		return;
+	ft_bzero(padding, sizeof(int) * 4);
+	i = -1;
+	while (++i < fil->end)
+		find_padding(padding, ((t_ls *)fil->contents[i])->statinfo);
+	i = -1;
+	while (++i < fil->end)
+	{
+		print_long((t_ls *)fil->contents[i], padding);
+		write(1, "\n", 1);
 	}
+	write(1, "\n", 1);
+}
 
-	/*
-	* If not recursing down this tree and don't need stat info, just get
-	* the names.
-	*/
-	ch_options = !g_ls_flg->recursive && options & FTS_NOSTAT ? FTS_NAMEONLY : 0;
+static void		handle_files(t_arr *fil)
+{
 
-	while ((p = fts_read(ftsp)) != NULL)
-		switch (p->fts_info) {
-			case FTS_DC:
-				warnx("%s: directory causes a cycle", p->fts_name);
-				if (COMPAT_MODE("bin/ls", "Unix2003")) {
-					rval = 1;
-				}
-				break;
-			case FTS_DNR:
-			case FTS_ERR:
-				warnx("%s: %s", p->fts_name, strerror(p->fts_errno));
-				rval = 1;
-				break;
-			case FTS_D:
-				if (p->fts_level != FTS_ROOTLEVEL &&
-						p->fts_name[0] == '.' && !g_ls_flg->listdot)
-					break;
+	if (fil->end > 0)
+		ls_sort(fil);
+	if (g_ls_flags & FLG_l)
+		print_custom_l(fil);
+	else if (g_ls_flags & FLG_1)
+		ls_print_simple(fil);
+	else
+		ls_print_col(fil);
+}
 
-				/*
-				* If already output something, put out a newline as
-				* a separator.  If multiple arguments, precede each
-				* directory with its name.
-				*/
+static void		handle_dir(t_arr *dir)
+{
+	int		i;
 
-				if (output)
-					(void)printf("\n%s:\n", p->fts_path);
-				else if (argc > 1) {
-					(void)printf("%s:\n", p->fts_path);
-					output = 1;
-				}
-				chp = fts_children(ftsp, ch_options);
-				if (COMPAT_MODE("bin/ls", "Unix2003") && ((options & FTS_LOGICAL)!=0)) {
-					FTSENT *curr;
-					for (curr = chp; curr; curr = curr->fts_link) {
-						if (curr->fts_info == FTS_SLNONE)
-							curr->fts_number = NO_PRINT;
-					}
-				}
-				display(p, chp);
+	if (dir->end == 0)
+		return ;
+	if (dir->end == 1)
+	{
+		ls_print_dir(((t_ls *)dir->contents[0])->path);
+		return ;
+	}
+	i = 0;
+	while (i < dir->end)
+	{
+		ft_printf("%s:\n", ((t_ls *)dir->contents[i])->name);
+		ls_print_dir(((t_ls *)dir->contents[i])->path);
+		if ((++i) < dir->end)
+			write(1, "\n", 1);
+	}
+}
 
-				if (!g_ls_flg->recursive && chp != NULL)
-					(void)fts_set(ftsp, p, FTS_SKIP);
-				break;
-			case FTS_SLNONE:	/* Same as default unless Unix conformance */
-				if (COMPAT_MODE("bin/ls", "Unix2003")) {
-					if ((options & FTS_LOGICAL)!=0) {	/* -L was specified */
-						warnx("%s: %s", p->fts_name, strerror(p->fts_errno ?: ENOENT));
-						rval = 1;
-					}
-				}
-				break;
-			default:
-				break;
+static void		handle_naf(t_arr *naf)
+{
+	int		i;
+
+	ls_sort(naf);
+	i = -1;
+	while (++i < naf->end)
+		ft_dprintf(2, "ls: %s: %s\n", ((t_ls *)naf->contents[i])->name, strerror(errno));
+}
+
+int8_t			ls_traverse(int i, int argc, char **argv)
+{
+	t_arr			*naf;
+	t_arr			*dir;
+	t_arr			*fil;
+	t_ls		*tmp;
+
+	fil = arr_create(sizeof(t_ls), 10);
+	MEMCHECK(fil);
+	dir = arr_create(sizeof(t_ls), 10);
+	MEMCHECK(dir);
+	naf = arr_create(sizeof(t_ls), 10);
+	MEMCHECK(naf);
+	fil->del = &file_del;
+	naf->del = &file_del;
+	dir->del = &file_del;
+
+	while (++i < argc)
+	{
+		tmp = ft_memalloc(sizeof(t_ls));
+		MEMCHECK(tmp);
+		tmp->path = ft_strdup(argv[i]);
+		tmp->name = ft_strdup(get_basename(argv[i]));
+		if (stat(argv[i], &tmp->statinfo) == -1)
+		{
+//			DEBUG("NAF");
+			arr_push(naf, tmp);
+			continue;
 		}
-	error = errno;
-	fts_close(ftsp);
-	errno = error;
-
-	if (errno)
-		err(1, "fts_read");
+		if (S_ISDIR(tmp->statinfo.st_mode))
+		{
+//			DEBUG("DIR");
+			arr_push(dir, tmp);
+		}
+		else
+		{
+//			DEBUG("FILE");
+			arr_push(fil, tmp);
+		}
+	}
+	handle_naf(naf);
+	handle_files(fil);
+	handle_dir(dir);
+	arr_del(dir);
+	arr_del(naf);
+	arr_del(fil);
+	return (0);
 }
